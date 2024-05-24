7119c9356fcb886b9806b9cd4814d0a4309aac68
Hyejin <hejin8307@naver.com>
2024-05-24 13:22:15 +0900

    :sparkles: Splash 화면 추가

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { NotFound, Splash } from "./pages";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Splash /> },
+  {
+    path: "/home",
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
