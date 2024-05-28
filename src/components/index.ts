117f4f01d6b4aac2b75fb31f9a53a96c3122e5ff
seoyeon08 <silversun0808@gmail.com>
2024-05-28 14:39:29 +0900

    ✨ AccountBoard 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar";
 export { default as NavIcons } from "./common/NavIcon";
 export { default as Modal } from "./Modal";
+export { default as AccountBoard } from "./AccountBoard";
