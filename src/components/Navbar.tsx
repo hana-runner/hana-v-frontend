ff883ecebd9f27df4f2f4ae030f2024cc115f305
jennyjoo <165225479+jennyjoo@users.noreply.github.com>
2024-05-30 13:42:13 +0900

    design: 아이디 찾기 프로세스 화면 정렬 (#23)
    :lipstick: 화면상 컴포넌트 정렬

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,6 @@ interface NavbarType {
   option: boolean;
 }
 
-
 const Navbar = ({ title, option }: NavbarType) => {
   const navigate = useNavigate();
 
--- src/pages/user/findId/ShowId.tsx
+++ src/pages/user/findId/ShowId.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Verified from "./Verified";
 
 const ShowId = () => {
+  const [verified, setVerified] = useState(false);
+
   return (
-    <section className="flex flex-col justify-evenly items-center h-[100vh] gap-9">
-      <div className="bg-hanaSilver rounded-lg text-center items-center w-80 h-14">
+    <section className="flex flex-col gap-10 w-80 h-80 py-10">
+      <h1 className="text-hanaBlack text-lg font-hanaMedium">
+        아이디를 찾았어요!
+      </h1>
+      <div className="bg-hanaSilver bg-hanaSilver bg-opacity-15 rounded-lg text-center text-balance w-80 h-10">
         아이디
+      </div>
+      <div className="flex flex-col gap-5">
+        <button
+          type="button"
+          className="underline text-hanaSilver font-hanaRegular text-sm underline-offset-2"
+        >
+          비밀번호 재등록
+        </button>
+        <button type="button" className="btn-primary">
+          로그인
+        </button>
       </div>
     </section>
   );
