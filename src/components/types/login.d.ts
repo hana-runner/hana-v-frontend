28477a59a36791562002d9c68f0de09b18413f56
Hyejin <31836035+hejin8307@users.noreply.github.com>
2024-05-30 17:38:14 +0900

    Design: 화면 레이아웃 버그 해결 및 스크롤 막기 구현 (#26)
    💄 화면 스크롤 버그 수정
    
    🔀 Merge from dev
    
    🐛 충돌 해결
    
    🔥 Types 폴더 이동

--- src/components/types/login.d.ts
+++
@@ -1,9 +0,0 @@
-type LoginValidateInfo = {
-  userId: boolean;
-  userPw: boolean;
-};
-
-type LoginType = Pick<UserInfoType, "username" | "pw">;
-
-type FindIdType = Pick<UserInfoType, "email" | "name">;
-type FindPwType = Pick<UserInfoType, "email" | "username" | "name">;
