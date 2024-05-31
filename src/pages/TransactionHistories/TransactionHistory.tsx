e772a4bffacfbcab4d857ab354329e30784ea1d5
seoyeon08 <silversun0808@gmail.com>
2024-05-31 11:17:46 +0900

    🐛 TransactionHistory 페이지의 Navbar logo 에러 해결

--- src/pages/TransactionHistories/TransactionHistory.tsx
+++ src/pages/TransactionHistories/TransactionHistory.tsx
@@ -6,7 +6,7 @@ import TransactionHistoryList from "../../components/TransactionHistoryList";
 function TransactionHistory() {
   return (
     <section className="flex flex-col items-center flex-wrap">
-      <Navbar title="거래내역조회" option={false} />
+      <Navbar title="거래내역조회" option={false} logo={false} />
       <AccountBoard />
       <HistoryOption />
       <TransactionHistoryList />
