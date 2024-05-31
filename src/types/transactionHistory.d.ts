4170e3e191d2bf86f6fbcdeb27da3f2859f64ff1
seoyeon08 <silversun0808@gmail.com>
2024-05-31 10:24:49 +0900

    ✨ 거래내역 더미데이터 추가 & 거래목록 20개씩 출력

---
+++ src/types/transactionHistory.d.ts
@@ -0,0 +1,14 @@
+export interface Transaction {
+  id: number;
+  account_id: number;
+  user_id: number;
+  category_id: number;
+  num: number;
+  type: boolean;
+  description: string;
+  action: string;
+  amount: number;
+  balance: number;
+  created_at: string;
+  updated_at: string;
+}
