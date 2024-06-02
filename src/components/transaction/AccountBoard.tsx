bf12cd454a352ad39a382424874ec4ceadd476bf
seoyeon Moon <66253833+seoyeon08@users.noreply.github.com>
2024-06-02 23:59:34 +0900

    design: 달력 커스터마이징 & Transaction 페이지 디자인 수정 (#59)
    * 💄 AccountBoard shadow 수정 xl > md
    
    * 💄 Calendar UI 수정

--- src/components/transaction/AccountBoard.tsx
+++ src/components/transaction/AccountBoard.tsx
@@ -13,7 +13,7 @@ const AccountBoard: React.FC = () => {
   const formattedTotalBalance = account.totalBalance.toLocaleString();
 
   return (
-    <div className="w-[326px] h-[145px] m-[20px] p-[18px] text-left shadow-xl rounded-[20px] bg-white">
+    <div className="w-[326px] h-[145px] m-[20px] p-[18px] text-left shadow-md rounded-[20px] bg-white">
       <div>
         <div className="flex items-center">
           <img src="/img/logo3.png" alt="logo3" className="w-[32px] h-[32px]" />
