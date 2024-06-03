d4e846ebcc1a4248b888c5c510822b2535290727
seoyeon Moon <66253833+seoyeon08@users.noreply.github.com>
2024-06-03 10:18:47 +0900

    feat: account id에 해당하는 계좌 정보 조회 기능 구현 (#64)
    * 📝 AccountsData.json 파일 추가
    
    * ✨ 계좌 id 별 계좌 정보 조회 구현

---
+++ src/types/account.d.ts
@@ -0,0 +1,10 @@
+interface accountInfoType {
+  accounts: Array;
+  id: number;
+  user_id: number;
+  account_number: number;
+  balance: number;
+  account_name: string;
+}
+
+export default accountInfoType;
