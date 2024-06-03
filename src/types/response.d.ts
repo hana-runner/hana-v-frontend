1a44ceebf9ce95a62505b81b8b512ad14fda8488
Hyejin <31836035+hejin8307@users.noreply.github.com>
2024-06-03 14:10:42 +0900

    refactor: Base response type 수정 (#65)
    ♻️ Base response type 수정

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -1,16 +1,24 @@
+<<<<<<< Updated upstream
 enum STATUS {
   OK = "ok",
   FAIL = "fail",
 }
 
 interface BasicApiType {
+=======
+interface BaseResponseType {
+>>>>>>> Stashed changes
   code: string;
   message: string;
   status: number;
   success: boolean;
   timestamp: string;
 }
+<<<<<<< Updated upstream
 
 interface BasicResultApiType<T> extends Required<BasicApiType> {
+=======
+interface ApiResponseType<T> extends Required<BasicApiType> {
+>>>>>>> Stashed changes
   data: T;
 }
