b6ba71dabd27cd5fa063b71423c309a6221afe2a
jennyjoo <100cllr@gmail.com>
2024-06-05 14:38:27 +0900

    :twisted_rightwards_arrows: merge dev

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -20,3 +20,4 @@ export { default as TransactionDetail } from "./TransactionDetail";
 export { default as Register } from "./user/register/Regstier";
 export { default as FindPw } from "./user/findPw/FindPw";
 export { default as Settings } from "./Settings";
+export { default as Notification } from "./notification/Notification";
