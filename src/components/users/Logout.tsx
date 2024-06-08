8b5fa5af376e0be4dd0c6c95c5feade11301f200
jennyjoo <100cllr@gmail.com>
2024-06-08 21:10:36 +0900

    :sparkles: 로그아웃 컴포넌트 구현

---
+++ src/components/users/Logout.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { useNavigate } from "react-router-dom";
+import { removeCookie } from "../../utils/cookie";
+
+interface Prop {
+  className?: string;
+  text?: string;
+}
+
+const Logout = ({ className = "", text = "로그아웃" }: Prop) => {
+  const navigate = useNavigate();
+  return (
+    <button
+      className={className}
+      type="button"
+      onClick={() => {
+        removeCookie("x-access-token", { path: "/" });
+        removeCookie("x-auth-token", { path: "/" });
+        navigate("/", { replace: true });
+      }}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Logout;
