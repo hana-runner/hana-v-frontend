efc99a27b0f999bd1dc96e33e17170de125e3ee1
seoyeon08 <silversun0808@gmail.com>
2024-06-09 23:36:23 +0900

    ✨ 사용자 별 실제 계좌 정보 조회 기능 추가

--- src/components/account/AccountInfo.tsx
+++ src/components/account/AccountInfo.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { BsTrash3 } from "react-icons/bs";
 
-function AccountInfo() {
+interface AccountInfoProps {
+  accountTitle: string;
+  accountNumber: string;
+  accountBankName: string;
+}
+
+function AccountInfo({
+  accountTitle,
+  accountNumber,
+  accountBankName,
+}: AccountInfoProps) {
   return (
     <div className="w-[348px] h-[156px] bg-white rounded-[24px] p-[20px] mt-[24px]">
       <div className="flex justify-between border-b-2">
-        <p className="text-[18px] font-hanaMedium">계좌명</p>
-        <BsTrash3 className="text-hanaSilver" />
+        <p className="text-[18px] font-hanaMedium">{accountTitle}</p>
+        <BsTrash3 className="text-hanaSilver cursor-pointer" />
       </div>
-      <p className="mt-[12px] text-left text-[16px]">계좌번호</p>
-      <p className="text-[17px] text-right mt-[28px]">은행명</p>
+      <p className="mt-[12px] text-left text-[16px]">{accountNumber}</p>
+      <p className="text-[17px] text-right mt-[28px]">{accountBankName}</p>
     </div>
   );
 }
