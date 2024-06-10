0ac2d9df7ee3afd41c6277489f5382b348b0c6c4
jennyjoo <100cllr@gmail.com>
2024-06-10 18:43:37 +0900

    :recycle: 코드 리팩토링

--- src/components/PushNotification.tsx
+++ src/components/PushNotification.tsx
@@ -52,24 +52,54 @@ const messaging = getMessaging(firebaseapp);
 //   }
 // });
 
+// async function handleAllowNotification() {
+//   registerServiceWorker(); // 나중에 설명
+//   try {
+//     const permission = await Notification.requestPermission();
+
+//     if (permission === "granted") {
+//       const token = await getToken(messaging, {
+//         vapidKey: VAPID_PUBLIC_KEY,
+//       });
+//       if (token) {
+//         console.log("token : ", token); // (토큰을 서버로 전송하는 로직)
+//       } else {
+//         alert("토큰 등록이 불가능 합니다. 생성하려면 권한을 허용해주세요");
+//       }
+//     } else if (permission === "denied") {
+//       alert(
+//         "web push 권한이 차단되었습니다. 알림을 사용하시려면 권한을 허용해주세요",
+//       );
+//     }
+//   } catch (error) {
+//     console.error("푸시 토큰 가져오는 중에 에러 발생", error);
+//   }
+// }
+
 async function handleAllowNotification() {
   registerServiceWorker(); // 나중에 설명
   try {
-    const permission = await Notification.requestPermission();
-
-    if (permission === "granted") {
-      const token = await getToken(messaging, {
-        vapidKey: VAPID_PUBLIC_KEY,
-      });
-      if (token) {
-        console.log("token : ", token); // (토큰을 서버로 전송하는 로직)
-      } else {
-        alert("토큰 등록이 불가능 합니다. 생성하려면 권한을 허용해주세요");
+    Notification.requestPermission().then((per) => {
+      if (per === "granted") {
+        getToken(messaging, {
+          vapidKey: VAPID_PUBLIC_KEY,
+        }).then((currToke) => {
+          if (currToke) {
+            console.log("token : ", currToke); // (토큰을 서버로 전송하는 로직)
+          } else {
+            alert("그딴거 없음");
+          }
+        });
       }
-    } else if (permission === "denied") {
-      alert(
-        "web push 권한이 차단되었습니다. 알림을 사용하시려면 권한을 허용해주세요",
-      );
+    });
+
+    const token = await getToken(messaging, {
+      vapidKey: VAPID_PUBLIC_KEY,
+    });
+    if (token) {
+      console.log("token : ", token); // (토큰을 서버로 전송하는 로직)
+    } else {
+      alert("토큰 등록이 불가능 합니다. 생성하려면 권한을 허용해주세요");
     }
   } catch (error) {
     console.error("푸시 토큰 가져오는 중에 에러 발생", error);
