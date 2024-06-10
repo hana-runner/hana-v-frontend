2ad4a5830de7dc8355d7557e1cc5ed5564f44acb
Hyejin <hejin8307@naver.com>
2024-06-10 17:09:02 +0900

    ✨ 관심사에 사용한 금액 프로세스 바, 6개월치 관심사 소비 금액 그래프 구현

--- src/pages/interest/InterestTransaction.tsx
+++ src/pages/interest/InterestTransaction.tsx
@@ -1,51 +1,49 @@
-import { useOutletContext } from "react-router-dom";
 import { useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
 
 const InterestTransaction = () => {
-  const { userInterestTransactions } = useOutletContext();
+  const [userInterestTransactions, date] = useOutletContext();
+
+  if (userInterestTransactions?.data.interestTotalSpent === 0) {
+    return <div>{date.month}월 사용한 금액이 없습니다.</div>;
+  }
 
   useEffect(() => {
     console.log(userInterestTransactions);
   }, [userInterestTransactions]);
 
   return (
-    <>
-      {userInterestTransactions &&
-        userInterestTransactions.data &&
-        userInterestTransactions.data.transaction.length > 0 && (
-          <ul className="flex flex-col px-6 border border-hanaSilver rounded-3xl">
-            {userInterestTransactions.data.transaction.map(
-              (transaction: UserInterestTransactionType, idx: number) => (
-                <li
-                  key={transaction.transactionHistoryId}
-                  className={`${
-                    idx !== userInterestTransactions.data.transaction.length - 1
-                      ? "border-b border-hanaSilver"
-                      : ""
-                  }`}
-                >
-                  <div className="my-6">
-                    <div className="text-[12px] text-hanaSilver text-left">
-                      {new Date(transaction.createdAt).toLocaleString()}
-                    </div>
-                    <div className="flex w-full">
-                      <div className="w-4/6 line-clamp-1 text-left">
-                        {transaction.description}
-                      </div>
-                      <div className="w-2/6 text-right text-hanaRed font-hanaMedium">
-                        -{transaction.amount.toLocaleString("KR-kr")}원
-                      </div>
-                    </div>
-                    <div className="text-[12px] text-hanaSilver text-right">
-                      {transaction.accountNumber}
-                    </div>
-                  </div>
-                </li>
-              ),
-            )}
-          </ul>
-        )}
-    </>
+    <ul className="flex flex-col px-6 border border-hanaSilver rounded-3xl">
+      {userInterestTransactions?.data.transaction.map(
+        (transaction: UserInterestTransactionType, idx: number) => (
+          <li
+            key={transaction.transactionHistoryId}
+            className={`${
+              idx !== userInterestTransactions.data.transaction.length - 1
+                ? "border-b border-hanaSilver"
+                : ""
+            }`}
+          >
+            <div className="my-6">
+              <div className="text-[12px] text-hanaSilver text-left">
+                {new Date(transaction.createdAt).toLocaleString()}
+              </div>
+              <div className="flex w-full">
+                <div className="w-4/6 line-clamp-1 text-left">
+                  {transaction.description}
+                </div>
+                <div className="w-2/6 text-right text-hanaRed font-hanaMedium">
+                  -{transaction.amount.toLocaleString("KR-kr")}원
+                </div>
+              </div>
+              <div className="text-[12px] text-hanaSilver text-right">
+                {transaction.accountNumber}
+              </div>
+            </div>
+          </li>
+        ),
+      )}
+    </ul>
   );
 };
 
