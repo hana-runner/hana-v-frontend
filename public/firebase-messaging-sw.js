da7a35392cfc3ff9ccf2d4f31c1188934070d2f8
jennyjoo <100cllr@gmail.com>
2024-06-10 18:23:29 +0900

    :recycle: 코드 리팩토링

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,16 +1,37 @@
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js",
+  "https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js",
 );
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js",
+  "https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js",
 );
-firebase.initializeApp({
-  apiKey: "AIzaSyAPBVQKxlDw1YgvoR1mJMXg9MWMxEhlHVU",
-  authDomain: "hana-v.firebaseapp.com",
-  projectId: "hana-v",
-  storageBucket: "hana-v.appspot.com",
-  messagingSenderId: "772014999430",
-  appId: "1:772014999430:web:31607b83737468898e8e07",
-  measurementId: "G-6NR609FYVD",
+
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
 });
+
+self.addEventListener("activate", function (e) {
+  console.log("fcm service worker가 실행되었습니다.");
+});
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdWVKTE3q17G6lRtF_D2xmi0zPxxLsYCY",
+  authDomain: "hanav-3f6ee.firebaseapp.com",
+  projectId: "hanav-3f6ee",
+  storageBucket: "hanav-3f6ee.appspot.com",
+  messagingSenderId: "964360216393",
+  appId: "1:964360216393:web:7af3e9a9ec433897ae4498",
+  measurementId: "G-2Z0TK05PLK",
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.title;
+  const notificationOptions = {
+    body: payload.body,
+    // icon: payload.icon
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
--- src/components/PushNotification.tsx
+++ src/components/PushNotification.tsx
@@ -1,23 +1,90 @@
 import { useEffect } from "react";
-import firebase from "firebase/app";
-import "firebase/messaging";
+import firebase from "firebase/compat/app";
+import { getMessaging } from "firebase/messaging/sw";
+import { getToken } from "firebase/messaging";
 import ApiClient from "../apis/apiClient";
 import { useModal } from "../context/ModalContext";
 
+<<<<<<< Updated upstream
 export const VAPID_PUBLIC_KEY = "BKCxoDymGFRQXp21d5FhA9ncs-BqMfT0FmC__3HzNmMX9m4veRjnlfhSTi0yBPVfn80O-KSvDMYSZzW5jfyKE7k";
 
+=======
+export const VAPID_PUBLIC_KEY =
+  "BKCxoDymGFRQXp21d5FhA9ncs-BqMfT0FmC__3HzNmMX9m4veRjnlfhSTi0yBPVfn80O-KSvDMYSZzW5jfyKE7k";
+>>>>>>> Stashed changes
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDdWVKTE3q17G6lRtF_D2xmi0zPxxLsYCY",
+  authDomain: "hanav-3f6ee.firebaseapp.com",
+  projectId: "hanav-3f6ee",
+  storageBucket: "hanav-3f6ee.appspot.com",
+  messagingSenderId: "964360216393",
+  appId: "1:964360216393:web:7af3e9a9ec433897ae4498",
+  measurementId: "G-2Z0TK05PLK",
 };
+<<<<<<< Updated upstream
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
+=======
+const firebaseapp = firebase.initializeApp(firebaseConfig);
+
+// import { sendTokenToServer } from "./api";
+
+function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker
+        .register("/firebase-messaging-sw.js")
+        .then(function (registration) {
+          console.log(
+            "Service Worker가 scope에 등록되었습니다.:",
+            registration.scope,
+          );
+        })
+        .catch(function (err) {
+          console.log("Service Worker 등록 실패:", err);
+        });
+    });
+  }
+}
+
+const messaging = getMessaging(firebaseapp);
+
+// onMessage(messaging, (payload) => {
+//   // console.log("알림 도착 ", payload);
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//   };
+
+//   if (Notification.permission === "granted") {
+//     new Notification(notificationTitle, notificationOptions);
+//   }
+// });
+
+async function handleAllowNotification() {
+  registerServiceWorker(); // 나중에 설명
+  try {
+    const permission = await Notification.requestPermission();
+
+    if (permission === "granted") {
+      const token = await getToken(messaging, {
+        vapidKey: VAPID_PUBLIC_KEY,
+      });
+      if (token) {
+        console.log("token : ", token); // (토큰을 서버로 전송하는 로직)
+      } else {
+        alert("토큰 등록이 불가능 합니다. 생성하려면 권한을 허용해주세요");
+      }
+    } else if (permission === "denied") {
+      alert(
+        "web push 권한이 차단되었습니다. 알림을 사용하시려면 권한을 허용해주세요",
+      );
+    }
+  } catch (error) {
+    console.error("푸시 토큰 가져오는 중에 에러 발생", error);
+  }
+>>>>>>> Stashed changes
 }
 
 const PushNotification: React.FC = () => {
@@ -64,6 +131,7 @@ const PushNotification: React.FC = () => {
   };
 
   useEffect(() => {
+<<<<<<< Updated upstream
     console.log(firebaseConfig);
 
     if (typeof window !== "undefined" && typeof window.navigator !== "undefined" && 'serviceWorker' in navigator && 'PushManager' in window) {
@@ -95,6 +163,40 @@ const PushNotification: React.FC = () => {
       console.warn("This browser does not support the required features.");
       openModal("Your browser does not support push notifications.");
     }
+=======
+    handleAllowNotification();
+    // console.log(firebaseConfig);
+    // try {
+    //   const messaging = firebase.messaging(firebaseapp);
+    //   Notification.requestPermission().then((permission) => {
+    //     if (permission === "granted") {
+    //       // 브라우저에서 알림 허용시
+    //       messaging
+    //         .getToken({
+    //           vapidKey: VAPID_PUBLIC_KEY,
+    //         })
+    //         .then((currentToken) => {
+    //           if (currentToken) {
+    //             console.log(currentToken);
+    //             // 토큰을 서버에 전달...
+    //             updateAlarmAtts(true, currentToken);
+    //           } else {
+    //             console.log(
+    //               "No registration token available. Request permission to generate one.",
+    //             );
+    //           }
+    //         })
+    //         .catch((err) => {
+    //           console.log("An error occurred while retrieving token. ", err);
+    //         });
+    //     } else {
+    //       console.log("Permission not granted for Notification");
+    //     }
+    //   });
+    // } catch (error) {
+    //   console.error("An error occurred while initializing messaging: ", error);
+    // }
+>>>>>>> Stashed changes
   }, []);
 
   return (
